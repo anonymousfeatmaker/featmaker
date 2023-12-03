ce67824398ab5323fc1943346ad7e31654e6fb25
anonymousfeatmakerauthor <151597642+anonymousfeatmakerauthor@users.noreply.github.com>
2023-12-03 09:57:37 +0900

    Update run_KLEEdefault.py

--- run_KLEEdefault.py
+++ run_KLEEdefault.py
@@ -41,10 +41,10 @@ if __name__=="__main__":
     parser.add_argument("--pgm", required=True)
     parser.add_argument("--exp_base", required=True)
     
-    parser.add_argument("--core", type=int, default=20)
+    parser.add_argument("--core", type=int, default=1)
     parser.add_argument("--time_budget", type=int, default=86400)
-    parser.add_argument("--base_time", type=int, default=120)
-    parser.add_argument("--n_heuristics", type=int, default=20)
+    parser.add_argument("--base_time", type=int, default=86400)
+    parser.add_argument("--n_heuristics", type=int, default=1)
     
     #Task options
     parser.add_argument("--main_task", default="featmaker", choices=["featmaker", "naive"])
